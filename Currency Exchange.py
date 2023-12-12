9e3b9bedb3b4b8966e1917b65e165c49f5a58347
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-12 22:49:59 +0700

    update to Currency Exchange apps

--- Currency Exchange.py
+++ Currency Exchange.py
@@ -1,3 +1,4 @@
+"""Currency exchange"""
 def exchange_money(budget, exchange_rate):
     """
     :param budget: float - amount of money you are planning to exchange.
@@ -11,7 +12,7 @@ def get_change(budget, exchanging_value):
     :param exchanging_value: float - amount of your money you want to exchange now.
     :return: float - amount left of your starting currency after exchanging.
     """
-    return (budget-exchanging_value)
+    return budget-exchanging_value
 def get_value_of_bills(denomination, number_of_bills):
     """
     :param denomination: int - the value of a bill.
@@ -25,14 +26,14 @@ def get_number_of_bills(amount, denomination):
     :param denomination: int - the value of a single bill.
     :return: int - number of bills that can be obtained from the amount.
     """
-    return (amount//denomination)
+    return amount//denomination
 def get_leftover_of_bills(amount, denomination):
     """
     :param amount: float - the total starting value.
     :param denomination: int - the value of a single bill.
     :return: float - the amount that is "leftover", given the current denomination.
     """
-    return (amount%denomination)
+    return amount%denomination
 def exchangeable_value(budget, exchange_rate, spread, denomination):
     """
     :param budget: float - the amount of your money you are planning to exchange.
@@ -44,4 +45,4 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     fee = (exchange_rate+(exchange_rate*(spread/100)))
     exchanged_money = exchange_money(budget,fee)
     bill_number = get_number_of_bills(exchanged_money,denomination)
-    return(get_value_of_bills(denomination, bill_number))
+    return get_value_of_bills(denomination, bill_number)
