19558d5fba53a043d5f26ded7c423c49431fc341
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-16 23:20:36 +0700

    Update Chessboard Grain.py

--- Numbers/Chessboard Grain/Chessboard Grain.py
+++ Numbers/Chessboard Grain/Chessboard Grain.py
@@ -1,8 +1,12 @@
 def square(number):
+    """Handle and throw ValueError if input is not as specified"""
     if number<1 or number>64:
         raise ValueError("square must be between 1 and 64")
-        
+
+    """Count the number of grains on designated tiles as determined by input number"""    
     grains = pow(2,number-1)
     return grains
+
 def total():
+    """Count the total number of grains on every tiles on the board"""
     return 2**64-1
