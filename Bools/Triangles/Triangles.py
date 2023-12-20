38f0d2e25ca46363c26abc97074da7717ae311ed
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-20 21:08:55 +0700

    Update Triangles.py
    Modify other functions

--- Bools/Triangles/Triangles.py
+++ Bools/Triangles/Triangles.py
@@ -5,26 +5,21 @@ def valid_triangle(sides):
     if a >= sides[2] and b >= sides[0] and c >= sides[1]:
         return True
 
+
 def equilateral(sides):
-    if sides[0]>0 and sides[1]>0 and sides[2]>0:
-        return bool(sides[0]==sides[1]==sides[2])
+    if sides[0] > 0 and sides[1] > 0 and sides[2] > 0:
+        return bool(sides[0] == sides[1] == sides[2])
     else:
         return False
-
+        
 def isosceles(sides):
-    a = sides[0]+sides[1]
-    b = sides[1]+sides[2]
-    c = sides[2]+sides[0]
-    if a>=sides[2] and b>=sides[0] and c>=sides[1]:
-        return bool(sides[0]==sides[1] or sides[0]==sides[2] or sides[1]==sides[2])
+    if valid_triangle(sides) == True:
+        return bool(sides[0] == sides[1] or sides[0] == sides[2] or sides[1] == sides[2])
     else:
         return False
-
+    
 def scalene(sides):
-    a = sides[0]+sides[1]
-    b = sides[1]+sides[2]
-    c = sides[2]+sides[0]
-    if a>=sides[2] and b>=sides[0] and c>=sides[1]:
-        return bool(sides[0]!=sides[1] and sides[1]!=sides[2] and sides[2]!=sides[0])
+    if valid_triangle(sides) == True:
+        return bool(sides[0] != sides[1] and sides[1] != sides[2] and sides[2] != sides[0])
     else:
         return False
