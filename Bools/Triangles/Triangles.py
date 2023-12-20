2f54c44d2aa43a192e8a533ef8e49acfe30081b8
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-20 20:24:53 +0700

    Update Triangles.py
    Add isosceles function

--- Bools/Triangles/Triangles.py
+++ Bools/Triangles/Triangles.py
@@ -3,3 +3,13 @@ def equilateral(sides):
         return bool(sides[0]==sides[1]==sides[2])
     else:
         return False
+
+def isosceles(sides):
+    a = sides[0]+sides[1]
+    b = sides[1]+sides[2]
+    c = sides[2]+sides[0]
+    if a>=sides[2] and b>=sides[0] and c>=sides[1]:
+        return bool(sides[0]==sides[1] or sides[0]==sides[2] or sides[1]==sides[2])
+    else:
+        return False
+
