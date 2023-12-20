e0971f5dfcd0c9f4ca2f6ad73f664e745fa08cc5
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-20 21:08:18 +0700

    Update Triangles.py
    Add valid_triangle function for check

--- Bools/Triangles/Triangles.py
+++ Bools/Triangles/Triangles.py
@@ -1,3 +1,10 @@
+def valid_triangle(sides):
+    a = sides[0] + sides[1]
+    b = sides[1] + sides[2]
+    c = sides[2] + sides[0]
+    if a >= sides[2] and b >= sides[0] and c >= sides[1]:
+        return True
+
 def equilateral(sides):
     if sides[0]>0 and sides[1]>0 and sides[2]>0:
         return bool(sides[0]==sides[1]==sides[2])
