f11530c75d4c426511bec9e0178bd7ac428560bc
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-21 20:07:57 +0700

    Update Meltdown Mitigation.py

--- Conditionals/Meltdown Mitigation/Meltdown Mitigation.py
+++ Conditionals/Meltdown Mitigation/Meltdown Mitigation.py
@@ -15,3 +15,52 @@ def is_criticality_balanced(temperature, neutrons_emitted):
         return True
     else:
         return False
+
+def reactor_efficiency(voltage, current, theoretical_max_power):
+    """Assess reactor efficiency zone.
+ 
+    :param voltage: int or float - voltage value.
+    :param current: int or float - current value.
+    :param theoretical_max_power: int or float - power that corresponds to a 100% efficiency.
+    :return: str - one of ('green', 'orange', 'red', or 'black').
+ 
+    Efficiency can be grouped into 4 bands:
+ 
+    1. green -> efficiency of 80% or more,
+    2. orange -> efficiency of less than 80% but at least 60%,
+    3. red -> efficiency below 60%, but still 30% or more,
+    4. black ->  less than 30% efficient.
+ 
+    The percentage value is calculated as
+    (generated power/ theoretical max power)*100
+    where generated power = voltage * current
+    """
+    efficiency = ((voltage*current)/theoretical_max_power)*100
+    if efficiency < 30:
+        return 'black'
+    elif efficiency < 60:
+        return 'red'
+    elif efficiency < 80:
+        return 'orange'
+    else:
+        return 'green'
+    
+def fail_safe(temperature, neutrons_produced_per_second, threshold):
+    """Assess and return status code for the reactor.
+ 
+    :param temperature: int or float - value of the temperature in kelvin.
+    :param neutrons_produced_per_second: int or float - neutron flux.
+    :param threshold: int or float - threshold for category.
+    :return: str - one of ('LOW', 'NORMAL', 'DANGER').
+ 
+    1. 'LOW' -> `temperature * neutrons per second` < 90% of `threshold`
+    2. 'NORMAL' -> `temperature * neutrons per second` +/- 10% of `threshold`
+    3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
+    """
+    burden = temperature*neutrons_produced_per_second 
+    if burden < threshold*(90/100):
+        return 'LOW'
+    elif (burden > threshold*(90/100) and burden <= threshold) or burden < threshold*(110/100):
+        return 'NORMAL'
+    else:
+        return 'DANGER'
