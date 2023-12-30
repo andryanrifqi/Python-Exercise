dafd8f4c33dab32d8b88123d33125cd8bd85cf51
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-30 20:27:08 +0700

    Update Basic Vocabulary.py

--- Strings/Basic Vocabulary/Basic Vocabulary.py
+++ Strings/Basic Vocabulary/Basic Vocabulary.py
@@ -6,3 +6,22 @@ def add_prefix_un(word):
     :return: str - of root word prepended with 'un'.
     """
     return 'un' + word
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+ 
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
+ 
+    This function takes a `vocab_words` list and returns a string
+    with the prefix and the words with prefix applied, separated
+     by ' :: '.
+ 
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
+    """
+    output = [vocab_words[0]]
+    for word in vocab_words[1:]:
+        output.append(vocab_words[0] + word)
+    return ' :: '.join(output)
