3da675a24609569eeadbffdbe5e139b115ac7d19
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-02 21:33:25 +0700

    Update Isogram.py

--- Strings/Isogram/Isogram.py
+++ Strings/Isogram/Isogram.py
@@ -1,6 +1,3 @@
 def is_isogram(string):
     chars = ''.join(char for char in string if char.isalnum()).lower()
-    for char in chars:
-        if (chars.count(char) <= 1) == False:
-            return False
-    return True
+    return len(chars)  == len(set(chars))
