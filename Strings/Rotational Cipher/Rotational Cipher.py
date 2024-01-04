7004e41cc74d320ca16d1031056a4b4e34bd43fc
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-04 22:06:15 +0700

    Update Rotational Cipher.py

--- Strings/Rotational Cipher/Rotational Cipher.py
+++ Strings/Rotational Cipher/Rotational Cipher.py
@@ -1,26 +1,11 @@
 import string
 
 def rotate(text, key):
-    alphabet_lower = list(string.ascii_lowercase)
-    alphabet_upper = list(string.ascii_uppercase)
-    
-    if 0 < key < 26:
-        cipher = list(text)
-        indx_in = 0
-        for char in cipher:
-            if not char.isalpha():
-                indx_in += 1
-            if char in alphabet_lower:
-                indx_out = alphabet_lower.index(char) + key
-                if indx_out > 25:
-                    indx_out = indx_out - 26
-                cipher[indx_in] = alphabet_lower[indx_out]
-                indx_in += 1
-            if char in alphabet_upper:
-                indx_out = alphabet_upper.index(char) + key
-                if indx_out > 25:
-                    indx_out -= 26
-                cipher[indx_in] = alphabet_upper[indx_out]
-                indx_in += 1
-        return ''.join(cipher)
-    return text
+    chars = (string.ascii_lowercase)
+    cipher = [chars[(index + key) % 26] for index in range(26)]
+
+    chars += chars.upper()
+    cipher.extend([char.upper() for char in cipher])
+
+    result = [cipher[chars.index(char)] if char in chars else char for char in text]
+    return ''.join(result)
