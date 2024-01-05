82aa2ba14362ebb4a91775d15286afa900cac4c2
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-05 22:47:45 +0700

    Create Black Jack.py

---
+++ Comparisons/Black Jack/Black Jack.py
@@ -0,0 +1,24 @@
+"""Functions to help play and score a game of blackjack.
+
+How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
+"Standard" playing cards: https://en.wikipedia.org/wiki/Standard_52-card_deck
+"""
+tens = ['10','J','Q','K']
+
+def value_of_card(card):
+    """Determine the scoring value of a card.
+
+    :param card: str - given card.
+    :return: int - value of a given card.  See below for values.
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 1
+    3.  '2' - '10' = numerical value.
+    """
+    if card in tens:
+        return 10
+
+    if card == 'A':
+        return 1
+        
+    return int(card)
