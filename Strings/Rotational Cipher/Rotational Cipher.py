8f151ece48e571b27fcc790ed8012ecf7bdda9c6
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-05 22:41:39 +0700

    Update Rotational Cipher.py

--- Strings/Rotational Cipher/Rotational Cipher.py
+++ Strings/Rotational Cipher/Rotational Cipher.py
@@ -2,10 +2,7 @@ import string
 
 def rotate(text, key):
     chars = (string.ascii_lowercase)
-    cipher = [chars[(index + key) % 26] for index in range(26)]
-
-    chars += chars.upper()
-    cipher.extend([char.upper() for char in cipher])
-
-    result = [cipher[chars.index(char)] if char in chars else char for char in text]
-    return ''.join(result)
+    
+    new_chars = chars[key:] + chars[:key]
+    cipher = str.maketrans((chars + chars.upper()), (new_chars + new_chars.upper()))
+    return text.translate(cipher)
