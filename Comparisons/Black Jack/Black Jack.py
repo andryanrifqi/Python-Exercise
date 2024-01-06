9453bdc92eb9d6b0c11ca18e04f77e49e8c16ed1
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-06 20:42:04 +0700

    Update Black Jack.py

--- Comparisons/Black Jack/Black Jack.py
+++ Comparisons/Black Jack/Black Jack.py
@@ -43,10 +43,10 @@ def higher_card(card_one, card_two):
 
 def value_of_ace(card_one, card_two):
     """Calculate the most advantageous value for the ace card.
- 
+
     :param card_one, card_two: str - card dealt. See below for values.
     :return: int - either 1 or 11 value of the upcoming ace card.
- 
+
     1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
@@ -59,19 +59,19 @@ def value_of_ace(card_one, card_two):
 
 def is_blackjack(card_one, card_two):
     """Determine if the hand is a 'natural' or 'blackjack'.
- 
+
     :param card_one, card_two: str - card dealt. See below for values.
     :return: bool - is the hand is a blackjack (two cards worth 21).
- 
+
     1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
-    return ((card_one == 'A' and card_two in tens) or (card_two == 'A' and card_one in tens))
+    return (card_one == 'A' and card_two in tens) or (card_two == 'A' and card_one in tens)
 
 def can_split_pairs(card_one, card_two):
     """Determine if a player can split their hand into two hands.
- 
+
     :param card_one, card_two: str - cards dealt.
     :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
     """
@@ -79,7 +79,7 @@ def can_split_pairs(card_one, card_two):
 
 def can_double_down(card_one, card_two):
     """Determine if a blackjack player can place a double down bet.
- 
+
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
     """
