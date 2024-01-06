cbb2d3096add800c69675b0b750e00807bcd9cb6
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-06 20:40:38 +0700

    Update Black Jack.py

--- Comparisons/Black Jack/Black Jack.py
+++ Comparisons/Black Jack/Black Jack.py
@@ -76,3 +76,11 @@ def can_split_pairs(card_one, card_two):
     :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
     """
     return value_of_card(card_one) == value_of_card(card_two)
+
+def can_double_down(card_one, card_two):
+    """Determine if a blackjack player can place a double down bet.
+ 
+    :param card_one, card_two: str - first and second cards in hand.
+    :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
+    """
+    return value_of_card(card_one) + value_of_card(card_two) in [9,10,11]
