24739c07cb8a30615a642a4a9159840c4767be07
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-08 22:47:54 +0700

    Update Darts.py

--- Comparisons/Darts/Darts.py
+++ Comparisons/Darts/Darts.py
@@ -1,12 +1,6 @@
 import math
 
 def score(x, y):
-    distance = math.sqrt(x**2+y**2)
+    distance = math.sqrt(x ** 2 + y ** 2)
     
-    if distance <= 1:
-        return 10
-    if 1 < distance <= 5:
-        return 5
-    if 5 < distance <= 10:
-        return 1
-    return 0
+    return (distance <= 1) * 5 + (distance <= 5) * 4 + (distance <= 10) * 1
