78c55eb4ec91e8fb359ab2476a8f35322c4dbd6d
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-19 20:57:02 +0700

    Update Atbash Cipher.py

--- String Methods/Atbash Cipher/Atbash Cipher.py
+++ String Methods/Atbash Cipher/Atbash Cipher.py
@@ -1,11 +1,12 @@
 import re
-encoder = str.maketrans("abcdefghijklmnopqrstuvwxyz", "zyxwvutsrqponmlkjihgfedcba")
-decoder = str.maketrans("zyxwvutsrqponmlkjihgfedcba", "abcdefghijklmnopqrstuvwxyz")
+from string import ascii_lowercase
+
+KEY = str.maketrans(ascii_lowercase, ascii_lowercase[::-1])
 atbash_len = 5
 
 def encode(plain_text):
-    text = re.sub("[^A-Z0-9]", "", plain_text,0,re.IGNORECASE).lower().translate(encoder)
+    text = re.sub("[^A-Z0-9]", "", plain_text,0,re.IGNORECASE).lower().translate(KEY)
     return " ".join(text[i:i+atbash_len] for i in range(0, len(text), atbash_len))
 
 def decode(ciphered_text):
-    return ciphered_text.replace(" ", "").lower().translate(decoder)
+    return ciphered_text.replace(" ", "").lower().translate(KEY)
