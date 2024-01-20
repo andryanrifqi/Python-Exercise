f09e1a15dd2fa36fc6a9e36f1a3aa27fcc52cc0e
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-20 20:52:30 +0700

    Update armstrong_numbers_test.py

--- Numbers/Armstrong Numbers/tests/armstrong_numbers_test.py
+++ Numbers/Armstrong Numbers/tests/armstrong_numbers_test.py
@@ -7,3 +7,27 @@ from armstrong_numbers import (
 class ArmstrongNumbersTest(unittest.TestCase):
     def test_zero_is_armstrong_number(self):
         self.assertIs(is_armstrong_number(0), True)
+
+    def test_single_digit_numbers(self):
+        self.assertIs(is_armstrong_number(9), True)
+
+    def test_two_digit_numbers(self):
+        self.assertIs(is_armstrong_number(11), False)
+
+    def test_three_digit_numbers_correct(self):
+        self.assertIs(is_armstrong_number(153), True)
+    
+    def test_three_digit_numbers_incorrect(self):
+        self.assertIs(is_armstrong_number(154), False)
+    
+    def test_four_digit_numbers_correct(self):
+        self.assertIs(is_armstrong_number(9474), True)
+    
+    def test_four_digit_numbers_incorrect(self):
+        self.assertIs(is_armstrong_number(9475), False)
+
+    def test_seven_digit_numbers_correct(self):
+        self.assertIs(is_armstrong_number(9926315), True)
+    
+    def test_seven_digit_numbers_incorrect(self):
+        self.assertIs(is_armstrong_number(9926314), False)
