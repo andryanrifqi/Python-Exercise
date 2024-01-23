26ac9c4bcbcd526e68f9ab6aee7e69fa262f3204
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-23 21:25:17 +0700

    Update Resistor Color Duo.py

--- Lists/Resistor Color Duo/Resistor Color Duo.py
+++ Lists/Resistor Color Duo/Resistor Color Duo.py
@@ -1,16 +1,16 @@
-resistor_colors = [
-            "black",
-            "brown",
-            "red",
-            "orange",
-            "yellow",
-            "green",
-            "blue",
-            "violet",
-            "grey",
-            "white",
-        ]
+resistor_colors = {
+            "black" : 0,
+            "brown" : 1,
+            "red" : 2,
+            "orange" : 3,
+            "yellow" : 4,
+            "green" : 5,
+            "blue" : 6,
+            "violet" : 7,
+            "grey" : 8,
+            "white" : 9,
+    }
     
 def value(colors):
 
-    return int(str(resistor_colors.index(colors[0])) + str(resistor_colors.index(colors[1])))
+    return int(str(resistor_colors[colors[0]]) + str(resistor_colors[colors[1]]))
