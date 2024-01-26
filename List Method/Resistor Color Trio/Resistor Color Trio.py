6b53589b8a0a08c0635f45373613335458c014d0
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-26 23:53:50 +0700

    Update Resistor Color Trio.py

--- List Method/Resistor Color Trio/Resistor Color Trio.py
+++ List Method/Resistor Color Trio/Resistor Color Trio.py
@@ -12,37 +12,17 @@ resistor_colors = {
     }
 
 def label(colors):
-    ohms_value = list(str((resistor_colors[colors[0]]*10 + resistor_colors[colors[1]]) * pow(10, resistor_colors[colors[2]])))
-    if ohms_value[0] == '0':
-        return '0 ohms'
+    ohms_value = (resistor_colors[colors[0]]*10 + resistor_colors[colors[1]]) * pow(10, resistor_colors[colors[2]])
+    prefix = ""
     
-    counter = 0
-    while ohms_value[-1] == '0':
-        ohms_value.pop(-1)
-        counter += 1
-
-    if counter == 0:
-        ohms_value.append(' ohms')
-    if counter == 1:
-        ohms_value.append('0 ohms')
-    if counter == 2:
-        ohms_value.append('00 ohms')
-    if counter == 3:
-        ohms_value.append(' kiloohms')
-    if counter == 4:
-        ohms_value.append('0 kiloohms')
-    if counter == 5:
-        ohms_value.append('00 kiloohms')    
-    if counter == 6:
-        ohms_value.append(' megaohms')
-    if counter == 7:
-        ohms_value.append('0 megaohms')
-    if counter == 8:
-        ohms_value.append('00 megaohms')
-    if counter == 9:
-        ohms_value.append(' gigaohms')
-    if counter == 10:
-        ohms_value.append('0 gigaohms')
-    
-    
-    return ''.join(ohms_value)
+    if ohms_value > 1_000_000_000:
+        prefix = "giga"
+        ohms_value //= 1_000_000_000
+    if ohms_value > 1_000_000:
+        prefix = "mega"
+        ohms_value //= 1_000_000
+    if ohms_value > 1_000:
+        prefix = "kilo"
+        ohms_value //= 1_000
+        
+    return f"{ohms_value} {prefix}ohms"
