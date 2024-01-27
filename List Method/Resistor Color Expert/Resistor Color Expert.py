b62c4b99e46e938598b87b1d7112c95197838688
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-27 18:06:07 +0700

    Update Resistor Color Expert.py

--- List Method/Resistor Color Expert/Resistor Color Expert.py
+++ List Method/Resistor Color Expert/Resistor Color Expert.py
@@ -12,31 +12,29 @@ resistor_colors = {
     }
 
 resistor_tolerance = {
-        "grey" : "0.05%",
-        "violet" : "0.1%",
-        "blue" : "0.25%",
-        "green" : "0.5%",
-        "brown" : "1%",
-        "red" : "2%",
-        "gold" : "5%",
-        "silver" : "10%",
+        "grey" : " ±0.05%",
+        "violet" : " ±0.1%",
+        "blue" : " ±0.25%",
+        "green" : " ±0.5%",
+        "brown" : " ±1%",
+        "red" : " ±2%",
+        "gold" : " ±5%",
+        "silver" : " ±10%",
     }
 
-def resistor_label(colors):
-    tolerance = ""
-    prefix = ""
-    
+def count_ohms_value(colors):
     if len(colors) == 1:
-        return "0 ohms"
-        
+        ohms_value = 0
     if len(colors) == 4:
         ohms_value = (resistor_colors[colors[0]]*10 + resistor_colors[colors[1]]) * pow(10, resistor_colors[colors[2]])
-        tolerance = resistor_tolerance[colors[3]]
-        
     if len(colors) == 5:
         ohms_value = (resistor_colors[colors[0]]*100 + resistor_colors[colors[1]]*10 + resistor_colors[colors[2]]) * pow(10, resistor_colors[colors[3]])
-        tolerance = resistor_tolerance[colors[4]]
         
+    return ohms_value
+
+def prefix_finder(colors):
+    ohms_value =  count_ohms_value(colors)
+    prefix = ""
     if ohms_value > 1_000_000_000:
         prefix = "giga"
         ohms_value /= 1_000_000_000
@@ -47,7 +45,31 @@ def resistor_label(colors):
         prefix = "kilo"
         ohms_value /= 1_000
 
+    return prefix
+    
+def tolerance_finder(colors):
+    if len(colors) == 1:
+        tolerance = ""
+    if len(colors) == 4:
+        tolerance = resistor_tolerance[colors[3]]
+    if len(colors) == 5:
+        tolerance = resistor_tolerance[colors[4]]
+
+    return tolerance
+
+def resistor_label(colors):
+    ohms_value = count_ohms_value(colors)
+    prefix = prefix_finder(colors)
+    tolerance = tolerance_finder(colors)
+    
+    if ohms_value > 1_000_000_000:
+        ohms_value /= 1_000_000_000
+    if ohms_value > 1_000_000:
+        ohms_value /= 1_000_000
+    if ohms_value > 1_000:
+        ohms_value /= 1_000
+        
     if type(ohms_value) is float and ohms_value.is_integer():
         ohms_value = int(ohms_value)
         
-    return f"{ohms_value} {prefix}ohms ±{tolerance}"
+    return f"{ohms_value} {prefix}ohms{tolerance}"
