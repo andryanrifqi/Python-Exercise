2d0e510987c7570ef4f9d7b5c24646edf4eab309
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-31 20:08:12 +0700

    Update Making the Grade.py

--- Loops/Making the Grade/Making the Grade.py
+++ Loops/Making the Grade/Making the Grade.py
@@ -7,12 +7,7 @@ def round_scores(student_scores):
     :param student_scores: list - float or int of student exam scores.
     :return: list - student scores *rounded* to nearest integer value.
     """
-    new_list = []
-
-    for score in student_scores:
-        new_list.append(round(score))
-
-    return new_list
+    return [round(score) for score in student_scores]
 
 def count_failed_students(student_scores):
     """Count the number of failing students out of the group provided.
